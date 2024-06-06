e172b257054c5614a7d15474a0638e271848a8c8
CHAN Suvannet <chansuvannet999@gmail.com>
2024-06-06 18:25:16 +0700

    add responsive header

--- web/src/app/main/cp/header/header.component.jsx
+++ web/src/app/main/cp/header/header.component.jsx
@@ -1,14 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./header.component.css";
+import SmallScreenHeader from "./small.componet";
 import HeaderFacebookImage from "/src/assets/logo/facebook.png";
-import HeaderImage from "/src/assets/logo/gif.gif";
 import HeaderInstagramImage from "/src/assets/logo/instagram.png";
+import HeaderImage from "/src/assets/logo/logo-header.gif";
 import HeaderTelegramImage from "/src/assets/logo/telegram.png";
 import HeaderTradImage from "/src/assets/logo/trad.png";
+import HeaderImageHover from "/src/assets/logo/video-unscreen.gif";
 
 const Header = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [imageSrc, setImageSrc] = useState(HeaderTradImage);
+  const [activeLink, setActiveLink] = useState(null);
+  const [isPhone, setIsPhone] = useState(window.innerWidth <= 600);
+  const [isiPad, setIsiPad] = useState(
+    window.innerWidth > 600 && window.innerWidth <= 980
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setIsPhone(width <= 600);
+      setIsiPad(width > 600 && width <= 980);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -19,131 +41,187 @@ const Header = () => {
     setDropdownOpen(false);
   };
 
-  return (
-    <header className="flex justify-between px-9 items-center bg-color-cus">
-      <nav className="header-nav text-gray-800 ">
-        <ul>
-          <li>
-            <a href="#faq" className="fire-animation ">
-              <img src={HeaderTradImage} className="w-6 h-6" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#skill" className="hover:text-red-300">Skills</a>
-          </li>
-          <li>
-            <a href="#portfolio" className="hover:text-red-300">Portfolio</a>
-          </li>
-          <li>
-            <a
-              className="flex justify-start items-center relative hover:text-red-300 "
-              id="dropdownDefaultButton"
-              data-dropdown-toggle="dropdown"
-              type="button"
-              onClick={toggleDropdown}
-            >
-              About Me{" "}
-              <svg
-                className={`w-2.5 h-2.5 ms-3 ${
-                  dropdownOpen ? "rotate-180" : ""
-                }`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 4 4 4-4"
+  const handleMouseEnter = () => {
+    setImageSrc(HeaderImageHover);
+  };
+
+  const handleMouseLeave = () => {
+    setImageSrc(HeaderTradImage);
+  };
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
+  if (isPhone) {
+    return <SmallScreenHeader />;
+  } else if (isiPad && !isPhone) {
+    return (
+      <header className="flex justify-between px-9 items-center bg-color-cus large-screen-header">
+        hello
+      </header>
+    );
+  } else {
+    return (
+      <header className="flex justify-between px-9 items-center bg-color-cus large-screen-header">
+        <nav className="header-nav text-gray-800">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#faq">
+                <img
+                  src={imageSrc}
+                  className="w-6 h-6"
+                  alt=""
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
                 />
-              </svg>
-            </a>
-            {dropdownOpen && (
-              <div
-                id="dropdown"
-                className="z-900  absolute bg-white divide-y divide-gray-500 rounded-lg border border-gray-100 shadow-sm w-52 m-[-10px] mt-1"
+              </a>
+            </li>
+            <li>
+              <a
+                href="#skill"
+                className={`hover:text-red-300 ${
+                  activeLink === "skill" ? "text-red-500" : ""
+                }`}
+                onClick={() => handleLinkClick("skill")}
               >
-                <ul className="flex flex-col text-sm text-gray-700 dark:text-gray-200">
-                  <li>
-                    <a
-                      href="#"
-                      className={`block px-4 text-gray-800 hover:text-red-300 pt-2 ${
-                        selectedItem === "Education" ? "text-red-500" : ""
-                      }`}
-                      onClick={() => handleItemClick("Education")}
-                    >
-                      Education
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className={`block px-4 text-gray-800 hover:text-red-300 ${
-                        selectedItem === "Volunteer Work" ? "text-red-500" : ""
-                      }`}
-                      onClick={() => handleItemClick("Volunteer Work")}
-                    >
-                      Volunteer Work
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className={`block px-4 text-gray-800 hover:text-red-300  ${
-                        selectedItem === "Experience" ? "text-red-500" : ""
-                      }`}
-                      onClick={() => handleItemClick("Experience")}
-                    >
-                      Experience
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className={`block px-4 text-gray-800 hover:text-red-300 pb-2 ${
-                        selectedItem === "Certificate" ? "text-red-500" : ""
-                      }`}
-                      onClick={() => handleItemClick("Certificate")}
-                    >
-                      Certificate
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </li>
-        </ul>
-      </nav>
-      <div className="header flex justify-start items-center fire-animation">
-        <img src={HeaderImage} alt="" className=""/>
-      </div>
-      <nav className="header-nav text-gray-500">
-        <ul>
-          <li>
-            <a href="#faq">
-              <img src={HeaderFacebookImage} className="w-6 h-6 " alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#faq">
-              <img src={HeaderInstagramImage} className="w-6 h-6 " alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#faq">
-              <img src={HeaderTelegramImage} className="w-6 h-6 " alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="" className="text-red-300 hover:text-gray-300">Quick Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+                Skills
+              </a>
+            </li>
+            <li>
+              <a
+                href="#portfolio"
+                className={`hover:text-red-300 ${
+                  activeLink === "portfolio" ? "text-red-500" : ""
+                }`}
+                onClick={() => handleLinkClick("portfolio")}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                className={`flex items-center hover:text-red-300 ${
+                  dropdownOpen
+                    ? "text-red-500"
+                    : activeLink === "about"
+                    ? "text-red-500"
+                    : ""
+                }`}
+                id="dropdownDefaultButton"
+                onClick={() => {
+                  toggleDropdown();
+                  handleLinkClick("about");
+                }}
+              >
+                About Me{" "}
+                <svg
+                  className={`w-2.5 h-2.5 ml-2 transform ${
+                    dropdownOpen ? "rotate-180" : ""
+                  }`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1l4 4 4-4"
+                  />
+                </svg>
+              </a>
+              {dropdownOpen && (
+                <div
+                  id="dropdown"
+                  className="absolute bg-white divide-y divide-gray-100 rounded-lg shadow-lg w-52 mt-2"
+                >
+                  <ul className="flex flex-col text-sm text-gray-700">
+                    <li>
+                      <a
+                        href="#"
+                        className={`block px-4 py-2 text-gray-800 hover:text-red-300 ${
+                          selectedItem === "Education" ? "text-red-500" : ""
+                        }`}
+                        onClick={() => handleItemClick("Education")}
+                      >
+                        Education
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className={`block px-4 py-2 text-gray-800 hover:text-red-300 ${
+                          selectedItem === "Volunteer Work"
+                            ? "text-red-500"
+                            : ""
+                        }`}
+                        onClick={() => handleItemClick("Volunteer Work")}
+                      >
+                        Volunteer Work
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className={`block px-4 py-2 text-gray-800 hover:text-red-300 ${
+                          selectedItem === "Experience" ? "text-red-500" : ""
+                        }`}
+                        onClick={() => handleItemClick("Experience")}
+                      >
+                        Experience
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className={`block px-4 py-2 text-gray-800 hover:text-red-300 ${
+                          selectedItem === "Certificate" ? "text-red-500" : ""
+                        }`}
+                        onClick={() => handleItemClick("Certificate")}
+                      >
+                        Certificate
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </li>
+          </ul>
+        </nav>
+        <div className="header flex items-center justify-center gap-1 fire-animation">
+          <h1>Suvannet</h1>
+          <img src={HeaderImage} alt="" className="w-6 h-6" />
+        </div>
+        <nav className="header-nav text-gray-500">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#faq">
+                <img src={HeaderFacebookImage} className="w-6 h-6" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="#faq">
+                <img src={HeaderInstagramImage} className="w-6 h-6" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="#faq">
+                <img src={HeaderTelegramImage} className="w-6 h-6" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="text-red-300 hover:text-gray-300">
+                Quick Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 };
 
 export default Header;
