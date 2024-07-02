9272373e4dbfa1e2bfa8f8701628b81ec7be5990
CHAN Suvannet <chansuvannet999@gmail.com>
2024-07-02 15:25:55 +0700

    test style

--- web/src/app/main/cp/content/about/about.component.jsx
+++ web/src/app/main/cp/content/about/about.component.jsx
@@ -1,8 +1,12 @@
 import Profile from "/src/assets/image/pic.jpg";
+import Coffee from "/src/assets/svg/cafe.svg";
+import Certificate from "/src/assets/svg/certificate.svg";
+import Code from "/src/assets/svg/code.svg";
+import Rocket from "/src/assets/svg/rocket.svg";
 const AboutMeComponent = () => {
   return (
-    <div className="flex justify-center items-center h-full px-9 shadow-sm pt-3">
-      <div className="w-full h-screen">
+    <div className="flex justify-center items-center h-full px-9">
+      <div className="w-full h-auto">
         <div className="flex justify-center pt-2">
           <h1 className="text-4xl font-bold max-600:text-2xl">About Me</h1>
         </div>
@@ -10,17 +14,19 @@ const AboutMeComponent = () => {
           <div className="max-600:hidden">
             <div>
               <div className="flex flex-col items-center justify-center">
-                <div className=" w-44 h-44 rounded-full flex justify-center items-center border-[10px] border-red-400 hover:border-red-500 hover:bg-red-200 cursor-pointer">
+                <div className=" w-44 h-44 rounded-full flex justify-center items-center border-[10px] border-red-300 hover:border-red-500 hover:bg-red-200 cursor-pointer">
                   <img
                     src={Profile}
                     alt={Profile}
                     className="w-40 h-40 rounded-full border-[2px] border-red-100 hover:scale-95"
                   />
                 </div>
-                <div className="flex flex-col justify-center items-center pt-5">
-                  <p className="pt-2 text-xl max-600:text-[18px]">
-                    Give me a cup of caffee{" "}
-                  </p>
+                <div className="flex flex-col justify-center items-center mt-3">
+                  <div className="p-2  rounded-[10px] text-red-300 cursor-pointer hover:bg-red-400 hover:text-white transition-colors duration-[900ms] ">
+                    <h1 className="text-[18px] max-600:text-[18px] font-bold">
+                      Downloaded CV
+                    </h1>
+                  </div>
                 </div>
               </div>
             </div>
@@ -29,19 +35,24 @@ const AboutMeComponent = () => {
             <div className="flex max-600:flex-col gap-2 max-600:gap-0">
               <div className="w-[50%] max-600:w-full">
                 <p className="text-xl max-600:text-base">
-                  I was born on September 20, 2003, in Svay Rieng province, I am
-                  known for my positive mindset, high motivation, excellent
-                  communication skills, and profound respect for capable
-                  individuals. Empower others by believing in their potential
-                  and celebrating their achievements.
+                  I am Suvannet, a computer science student from the Institute
+                  of Technology of Cambodia. I am known for my positive mindset,
+                  high motivation, excellent communication skills, and profound
+                  respect for capable individuals. I always focus on
+                  self-development and lifelong learning.
                 </p>
               </div>
-              <div className="max-980:hidden mb-5 mt-5"><hr /></div>
+              <div className="max-980:hidden mb-5 mt-5">
+                <hr />
+              </div>
               <div className="w-[50%] max-600:w-full">
                 <div>
                   <div>
                     <div className=" w-full">
-                      <p className="text-xl max-600:text-base">Development</p>
+                      <div className="flex justify-between">
+                        <p className="text-xl max-600:text-base">Development</p>
+                        <p className="text-xl max-600:text-base">80%</p>
+                      </div>
                       <div className="mb-5 pt-3">
                         <div className="bg-stroke bg-gray-200 relative h-2 w-full rounded-2xl max-600:h-1">
                           <div className="bg-red-400 absolute top-0 left-0 h-full w-4/5 rounded-2xl"></div>
@@ -49,7 +60,12 @@ const AboutMeComponent = () => {
                       </div>
                     </div>
                     <div className=" w-full">
-                      <p className="text-xl max-600:text-base">Problem-Solving</p>
+                      <div className="flex justify-between">
+                        <p className="text-xl max-600:text-base">
+                          Problem-Solving
+                        </p>
+                        <p className="text-xl max-600:text-base">60%</p>
+                      </div>
                       <div className="mb-5 pt-3 max-600:text-base">
                         <div className="bg-stroke bg-gray-200 relative h-2 w-full rounded-2xl max-600:h-1">
                           <div className="bg-yellow-400 absolute top-0 left-0 h-full w-4/6 rounded-2xl"></div>
@@ -57,7 +73,12 @@ const AboutMeComponent = () => {
                       </div>
                     </div>
                     <div className=" w-full">
-                      <p className="text-xl max-600:text-base">Communication</p>
+                      <div className="flex justify-between">
+                        <p className="text-xl max-600:text-base">
+                          Communication
+                        </p>
+                        <p className="text-xl max-600:text-base">91%</p>
+                      </div>
                       <div className="mb-5 pt-3">
                         <div className="bg-stroke bg-gray-200 relative h-2 w-full rounded-2xl max-600:h-1">
                           <div className="bg-purple-400 absolute top-0 left-0 h-full w-11/12 rounded-2xl"></div>
@@ -70,15 +91,43 @@ const AboutMeComponent = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-center items-center gap-2 pt-5">
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
+        <div className="flex justify-center items-center gap-44 mt-16 max-600:mt-5 max-600:mb-5 max-600:grid max-600:grid-cols-2 max-600:gap-4 max-600:pl-1">
+          <div className="flex gap-5 ">
+            <div>
+              <img src={Code} className="w-16 hover:scale-110 transition-transform duration-300 max-600:w-10" alt="" />
+            </div>
+            <div>
+              <h1 className="font-[800] text-2xl max-600:text-[18px]">6</h1>
+              <p className="mt-2 max-600:text-[14px] max-600:mt-0">Project Competed</p>
+            </div>
+          </div>
+          <div className="flex gap-5 ">
+            <div>
+              <img src={Rocket} className="w-16 hover:scale-110 transition-transform duration-300 max-600:w-10" alt="" />
+            </div>
+            <div>
+              <h1 className="font-bold text-2xl max-600:text-[18px]">6</h1>
+              <p className="mt-2 max-600:text-[14px] max-600:mt-0">Project Ongoing</p>
+            </div>
+          </div>
+          <div className="flex gap-5">
+            <div>
+              <img src={Coffee} className="w-16 hover:scale-110 transition-transform duration-300 max-600:w-10" alt="" />
+            </div>
+            <div>
+              <h1 className="font-bold text-2xl ">6</h1>
+              <p className="mt-2 max-600:text-[14px] max-600:mt-0">Cup of Coffee</p>
+            </div>
+          </div>
+          <div className="flex gap-5">
+            <div>
+              <img src={Certificate} className="w-16 hover:scale-110 transition-transform duration-300 max-600:w-10" alt="" />
+            </div>
+            <div>
+              <h1 className="font-bold text-2xl">6</h1>
+              <p className="mt-2 max-600:text-[14px] max-600:mt-0">Certificate</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- web/src/app/main/cp/content/home/home.component.jsx
+++ web/src/app/main/cp/content/home/home.component.jsx
@@ -7,32 +7,36 @@ import Mial from "/src/assets/svg/mail.svg";
 import Mouse from "/src/assets/svg/mouse.svg";
 const HomeComponent = () => {
   return (
-    <div className="flex justify-center items-center h-full px-9 shadow-sm">
+    <div className="flex justify-center items-center h-full px-9 ">
       <div className="w-full h-screen  flex justify-center items-center">
         <div className="flex flex-col justify-around h-full">
           <div>
             <div>
               <div className="flex flex-col items-center justify-center">
-                <div className="w-44 h-44 rounded-full flex justify-center items-center border-[10px] border-red-300 hover:border-red-400 hover:bg-red-200 cursor-pointer">
+                <div className="w-44 h-44 rounded-full flex justify-center items-center border-[10px] border-red-300 hover:bg-red-400 cursor-pointer">
                   <img
                     src={Profile}
                     alt={Profile}
-                    className="w-40 h-40 rounded-full border-[2px] border-red-100 hover:scale-95"
+                    className="w-40 h-40 rounded-full hover:scale-105 border-[2px]"
                   />
                 </div>
                 <div className="flex flex-col justify-center items-center pt-5">
-                  <h1 className="text-[34px] max-600:text-[28px]">Chan Suvannet</h1>
-                  <p className="pt-2 text-xl max-600:text-[18px]">I&apos;m Software Engineering </p>
+                  <h1 className="text-[34px] max-600:text-[28px]">
+                    Chan Suvannet
+                  </h1>
+                  <p className="pt-2 text-xl max-600:text-[18px]">
+                    I&apos;m Software Engineering{" "}
+                  </p>
                 </div>
               </div>
               <div className="flex justify-center items-center pt-5">
                 <div>
                   <ul className="flex space-x-4">
-                  <li>
+                    <li>
                       <a href="#faq">
                         <img
                           src={GitLab}
-                          className="w-6 h-6 hover:scale-90"
+                          className="w-7 h-7 hover:scale-90 transition-transform duration-300"
                           alt=""
                         />
                       </a>
@@ -41,7 +45,7 @@ const HomeComponent = () => {
                       <a href="#faq">
                         <img
                           src={GitHub}
-                          className="w-6 h-6 hover:scale-90"
+                          className="w-7 h-7 hover:scale-90 transition-transform duration-300"
                           alt=""
                         />
                       </a>
@@ -50,7 +54,7 @@ const HomeComponent = () => {
                       <a href="#faq">
                         <img
                           src={Discord}
-                          className="w-6 h-6 hover:scale-90"
+                          className="w-7 h-7 hover:scale-90 transition-transform duration-300"
                           alt="Instagram"
                         />
                       </a>
@@ -59,7 +63,7 @@ const HomeComponent = () => {
                       <a href="#faq">
                         <img
                           src={Mial}
-                          className="w-6 h-6 hover:scale-90"
+                          className="w-7 h-7 hover:scale-90 transition-transform duration-300"
                           alt=""
                         />
                       </a>
@@ -68,13 +72,15 @@ const HomeComponent = () => {
                 </div>
               </div>
               <div className="flex justify-center items-center pt-5">
-                <button className="bg-red-300 hover:bg-red-200 max-600:w-[145px] max-600:h-[44px] ">Hire Me</button>
+                <button className="bg-red-300 hover:bg-red-200 max-600:w-[145px] max-600:h-[44px] ">
+                  Hire Me
+                </button>
               </div>
             </div>
           </div>
           <div className="flex justify-center items-center pt-5 flex-col">
             <h1 className="text-xl">Scroll Down</h1>
-            <img className="mouse pt-10 cursor-pointer" src={Mouse}/>
+            <img className="mouse pt-10 cursor-pointer" src={Mouse} />
           </div>
         </div>
       </div>
